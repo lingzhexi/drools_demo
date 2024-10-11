500245801ad4d3113cc0b316d4728f96cb3395f8
geekcode <lingzhexi@163.com>
2024-10-11 11:50:22 +0800

    流程图：增加drools流程图

--- src/test/java/com/stormling/drools_demo/DroolsDemoApplicationTests.java
+++ src/test/java/com/stormling/drools_demo/DroolsDemoApplicationTests.java
@@ -15,18 +15,21 @@ class DroolsDemoApplicationTests {
 
     @Test
     public void test() {
+        //从Kie容器对象中获取会话对象
         KieSession kieSession = kieContainer.newKieSession();
 
+        // Fact 事实对象 Order
         Order order = new Order();
-        order.setAmount(99);
+        order.setAmount(1300);
 
+        // 将Order对象数据插入工作内存中
         kieSession.insert(order);
 
+        // 激活规则，由Drools框架自动进行规则匹配，如果规则匹配成功，则执行当前规则
         kieSession.fireAllRules();
 
-        //关闭会话
+        // 关闭会话
         kieSession.dispose();
-
         System.out.println("订单金额：" + order.getAmount() + "积分：" + order.getScore());
     }
 
